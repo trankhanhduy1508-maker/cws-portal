27698dd380fb595294dc67a14e2ace6060b99889
trankhanhduy1508-maker <trankhanhduy1508@gmail.com>
2026-08-04 12:05:43 +0700

    security: provide file intake auth dependencies

--- backend/src/files/files.module.ts
+++ backend/src/files/files.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { B2StorageService } from './b2-storage.service';
 import { GoogleDriveService } from './google-drive.service';
+import { SupabaseModule } from '../supabase/supabase.module';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
 
 @Module({
+  imports: [SupabaseModule],
   controllers: [FilesController],
-  providers: [B2StorageService, GoogleDriveService],
+  providers: [B2StorageService, GoogleDriveService, JwtAuthGuard],
   exports: [B2StorageService, GoogleDriveService],
 })
 export class FilesModule {}
