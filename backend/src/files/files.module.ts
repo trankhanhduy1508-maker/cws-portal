df3564b8c2dbea705abe6fed1e9a0337a5b3f4f9
trankhanhduy1508-maker <trankhanhduy1508@gmail.com>
2026-08-04 13:10:39 +0700

    feat: register resumable upload service

--- backend/src/files/files.module.ts
+++ backend/src/files/files.module.ts
@@ -4,11 +4,12 @@ import { B2StorageService } from './b2-storage.service';
 import { GoogleDriveService } from './google-drive.service';
 import { SupabaseModule } from '../supabase/supabase.module';
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { ResumableUploadService } from './resumable-upload.service';
 
 @Module({
   imports: [SupabaseModule],
   controllers: [FilesController],
-  providers: [B2StorageService, GoogleDriveService, JwtAuthGuard],
+  providers: [B2StorageService, GoogleDriveService, JwtAuthGuard, ResumableUploadService],
   exports: [B2StorageService, GoogleDriveService],
 })
 export class FilesModule {}
